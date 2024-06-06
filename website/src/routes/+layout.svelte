11a10ce3e21147a539231a46e93115ef4ccc5d2f
Alabhya Jindal <alabhya10@gmail.com>
2024-06-06 11:59:41 +0530

    dark

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import '../main.css';
+
 	function handleClick() {
-		// const isDark = document.body.classList.contains('dark');
-		document.body.classList.toggle('dark');
+		document.documentElement.classList.toggle('dark');
 	}
 </script>
 
@@ -16,59 +17,6 @@
 </section>
 
 <style>
-	:global(*) {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
-	}
-
-	:global(:root) {
-		--color-underwater-blue: #292d3e;
-		--color-white: #fefefc;
-		--color-faff-pink: #ffaff3;
-		--color-light-pink: #fedcfb;
-		--color-black: #1e1e1e;
-		--color-blacker: #151515;
-		--color-charcoal: #2f2f2f;
-		--color-unexpected-aubergine: #584355;
-		--color-aged-plastic-yellow: #fffbe8;
-
-		--background: var(--color-white);
-		--text: var(--color-black);
-		--border: var(--color-light-pink);
-	}
-
-	:global(html) {
-		color-scheme: light dark;
-	}
-
-	:global(body) {
-		font-family:
-			system-ui,
-			-apple-system,
-			BlinkMacSystemFont,
-			'Segoe UI',
-			Roboto,
-			Oxygen,
-			Ubuntu,
-			Cantarell,
-			'Open Sans',
-			'Helvetica Neue',
-			sans-serif;
-		line-height: 1.6;
-
-		background-color: var(--background);
-		color: var(--text);
-	}
-
-	@media (prefers-color-scheme: dark) {
-		:global(body) {
-			--background: var(--color-underwater-blue);
-			--text: var(--color-white);
-			--border: var(--color-unexpected-aubergine);
-		}
-	}
-
 	header {
 		background-color: var(--color-faff-pink);
 		color: var(--color-black);
@@ -94,24 +42,10 @@
 		color: inherit;
 	}
 
-	:global(a) {
-		color: inherit;
-	}
-
-	:global(a:hover) {
-		text-decoration: none;
-	}
-
 	section {
 		margin-top: 2em;
 		max-width: 600px;
 		margin-inline: auto;
 		padding-inline: 1em !important;
-	}
-
-	:global(body.dark) {
-		color-scheme: dark;
-		--background: var(--color-underwater-blue);
-		--text: var(--color-white);
 	}
 </style>
